ade15ecd19432b27afa8afceb0b68414130e93d1
Jullnar-Hajeh <160051567+Jullnar-Hajeh@users.noreply.github.com>
2026-01-10 22:36:29 +0200

    Add files via upload

---
+++ Components/Pages/LoginBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleLoginApp.Models;
+
+namespace SimpleLoginApp.Components.Pages
+{
+    public class LoginBase : ComponentBase
+    {
+        protected LoginModel loginModel = new LoginModel();
+        protected string message = string.Empty;
+        protected string messageCssClass = string.Empty;
+
+        protected void HandleLogin()
+        {
+            if (loginModel.Username.ToLower() == "admin" && loginModel.Password == "123456")
+            {
+                message = "Login successful!";
+                messageCssClass = "alert-success";
+            }
+            else
+            {
+                message = "Invalid credentials. Try: admin / 123456";
+                messageCssClass = "alert-danger";
+            }
+        }
+
+        protected void HandleInvalidSubmit()
+        {
+            message = string.Empty;
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleLoginApp.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
